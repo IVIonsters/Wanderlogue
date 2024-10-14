be539902fd89a7e2c0b275b3c7b9a017a258e2d4
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-13 21:02:22 -0500

    cityInfo
    added base city data file and create component substructure

---
+++ wanderlogue-frontend/src/utils/cityInfo.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: "Paris",
+    location: "France",
+    googleMapsUrl: "https://goo.gl/maps/3Pu3Kt9CUhx",
+    startDate: "5 Apr, 2023",
+    endDate: "12 Apr, 2023",
+    description: "Paris, the City of Lights, is renowned for its art, culture, and romance. From the grandeur of the Eiffel Tower to the charm of Montmartre, every corner of Paris tells a timeless story of beauty and elegance.",
+    imageUrl: "UPDATE URLS ZACHARY!"
+  },
+  {
+    title: "Rome",
+    location: "Italy",
+    googleMapsUrl: "https://goo.gl/maps/jNHw3uVV9zr",
+    startDate: "15 May, 2023",
+    endDate: "22 May, 2023",
+    description: "Rome, the Eternal City, is a living museum of ancient history and vibrant culture. Walk through centuries of art and architecture, from the grandeur of the Colosseum to the timeless beauty of the Vatican.",
+    imageUrl: "UPDATE URLS ZACHARY!"
+  },
+  {
+    title: "Athens",
+    location: "Greece",
+    googleMapsUrl: "https://goo.gl/maps/TdtdUE1QUHo",
+    startDate: "1 June, 2023",
+    endDate: "10 June, 2023",
+    description: "Athens, the cradle of Western civilization, blends ancient history with a dynamic modern city. From the iconic Acropolis to bustling streets filled with life, Athens is a journey through time and culture.",
+    imageUrl: "UPDATE URLS ZACHARY!"
+  }
+];
