0922434e0c8412ca456ba613596309f9ae3eb719
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-13 21:04:56 -0500

    Update cityInfo.js
    added imageURl's

--- wanderlogue-frontend/src/utils/cityInfo.js
+++ wanderlogue-frontend/src/utils/cityInfo.js
@@ -6,7 +6,7 @@ export default [
     startDate: "5 Apr, 2023",
     endDate: "12 Apr, 2023",
     description: "Paris, the City of Lights, is renowned for its art, culture, and romance. From the grandeur of the Eiffel Tower to the charm of Montmartre, every corner of Paris tells a timeless story of beauty and elegance.",
-    imageUrl: "UPDATE URLS ZACHARY!"
+imageUrl: "https://images.unsplash.com/photo-1522582324369-2dfc36bd9275?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Rome",
@@ -15,7 +15,7 @@ export default [
     startDate: "15 May, 2023",
     endDate: "22 May, 2023",
     description: "Rome, the Eternal City, is a living museum of ancient history and vibrant culture. Walk through centuries of art and architecture, from the grandeur of the Colosseum to the timeless beauty of the Vatican.",
-    imageUrl: "UPDATE URLS ZACHARY!"
+    imageUrl: " https://images.unsplash.com/photo-1598258500419-5d7895465a20?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
   {
     title: "Athens",
@@ -24,6 +24,6 @@ export default [
     startDate: "1 June, 2023",
     endDate: "10 June, 2023",
     description: "Athens, the cradle of Western civilization, blends ancient history with a dynamic modern city. From the iconic Acropolis to bustling streets filled with life, Athens is a journey through time and culture.",
-    imageUrl: "UPDATE URLS ZACHARY!"
+    imageUrl: "https://images.unsplash.com/photo-1555367615-632467610a5a?q=80&w=986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dv"
   }
 ];
