267595ba527da5c31378a30a4ba60aa9d0a7b942
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-13 21:19:01 -0500

    Navbar
    Setup base Navbar component and styling

---
+++ wanderlogue-frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="../../../public/images/Globe.png" alt="Wanderlogue Logo" />
+      <p>Wanderlogue Travel Journal</p>
+    </div>
+
+  )
+}
+
+export default Navbar
