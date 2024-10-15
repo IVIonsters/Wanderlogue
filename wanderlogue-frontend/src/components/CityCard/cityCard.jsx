9c3b8c50094d05b5ffa9d1502f6b686830c66d68
Zachary Polof <ZacharyPolof@gmail.com>
2024-10-14 21:19:46 -0500

    Update cityCard.jsx
    update city card with correct prop information to make cards show correct data.

--- wanderlogue-frontend/src/components/CityCard/cityCard.jsx
+++ wanderlogue-frontend/src/components/CityCard/cityCard.jsx
@@ -1,19 +1,21 @@
+/* eslint-disable react/prop-types */
 import "./cityCard.css";
 
-function CityCard(city) {
+function CityCard({city}) {
+
   console.log(city);
   return (
     <div className="cityCard">
-      <img className="cityImg" src="https://images.unsplash.com/photo-1522582324369-2dfc36bd9275?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Paris" />
+      <img className="cityImg" src={city.imageUrl} alt="Paris" />
       <div className="cityInfo">
         <div className="location">
           <img className="locationIcon" src="/images/location.png" alt="location icon" />
-          <p className="countryLocation">France</p>
-          <a className="googleMaps" href="https://example.com" target="_blank" rel="noopener noreferrer">View on Google Maps</a>
+          <p className="countryLocation">{city.location}</p>
+          <a className="googleMaps" href={city.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
         </div>
-        <h2 className="title">Eiffel Tower</h2>
-        <p className="dates">5 Apr, 2023 - 12 Apr, 2023</p>
-        <p className="description">Paris, the City of Lights, is renowned for its art, culture, and romance. From the grandeur of the Eiffel Tower to the charm of Montmartre, every corner of Paris tells a timeless story of beauty and elegance.</p>
+        <h2 className="title">{city.title}</h2>
+        <p className="dates">{city.startDate} - {city.endDate}</p>
+        <p className="description">{city.description}</p>
       </div>
     </div>
   );
